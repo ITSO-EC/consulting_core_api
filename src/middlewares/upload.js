fdcbc5aba8a871a19a3e45c343169352fefb9aba
JoelVeloz <joevpazm@espol.edu.ec>
2022-11-03 22:13:55 -0500

    updt

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,10 +1,10 @@
 const multer = require('multer');
-const path = require('path');
+const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        // cb(null, './uploads')
-        cb(null, path.join(__dirname, '../uploads/'));
+        cb(null, './uploads')
+        // cb(null, path.join(__dirname, '../uploads/'));
     },
 
     filename: function (req, file, cb) {
